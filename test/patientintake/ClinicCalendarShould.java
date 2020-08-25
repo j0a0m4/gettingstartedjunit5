f1120e83ef99968201975f499ebba8ec96f9f4ac
João Lopes <joaomarcoslopes@id.uff.br>
2020-08-25 16:39:56 -0300

    format

--- test/patientintake/ClinicCalendarShould.java
+++ test/patientintake/ClinicCalendarShould.java
@@ -3,10 +3,10 @@ package patientintake;
 import org.junit.jupiter.api.Test;
 
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Locale;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ClinicCalendarShould {
 
@@ -15,15 +15,17 @@ class ClinicCalendarShould {
       ClinicCalendar calendar = new ClinicCalendar();
       calendar.addAppointment("Jim", "Weaver", "avery",
          "09/01/2018 2:00 pm");
-      List<PatientAppointment> appointments = calendar.getAppointments();
+
+      var appointments = calendar.getAppointments();
       assertNotNull(appointments);
       assertEquals(1, appointments.size());
-      PatientAppointment enteredAppt = appointments.get(0);
-      assertEquals("Jim", enteredAppt.getPatientFirstName());
-      assertEquals("Weaver", enteredAppt.getPatientLastName());
-      assertEquals(Doctor.avery, enteredAppt.getDoctor());
+
+      var patient = appointments.get(0);
+      assertEquals("Jim", patient.getPatientFirstName());
+      assertEquals("Weaver", patient.getPatientLastName());
+      assertEquals(Doctor.avery, patient.getDoctor());
       assertEquals("9/1/2018 02:00 PM",
-         enteredAppt.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a", Locale.US)));
+         patient.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a", Locale.US)));
    }
 
 }
